93d07aec315a3ae2aca7a63fd82e63558ed29172
Silver303 <aghadimi94@gmail.com>
2026-09-17 14:47:46 -0700

    Update sketch.js

---
+++ Shapes/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(20);
+  fill(255, 120, 60);
+  circle(mouseX, mouseY, 50);
+  fill(255, 0, 60);
+  circle(400, 200, 100);
+  circle(500, 400, 200);
+  fill(90, 0, 300);
+  rect(100, 250, 200, 300);
+  fill(150, 30, 100);
+  rect(20, 50, 700, 10);
+  rect(20, 50, 10, 500);
+}
